dc0f3e3ce5d88839f17e0794370ecf84b3a7b58c
Xin Kai <tam.xin.kai@gmail.com>
2022-05-09 12:20:34 +0800

    fix agent route protection
    minor style changes

--- src/components/Properties/PropertyList.js
+++ src/components/Properties/PropertyList.js
@@ -10,10 +10,11 @@ import { useParams } from "react-router-dom";
 import PopupComponent from "./components/PopupComponent";
 import ImageViewer from "./components/ImageViewer";
 import Modal from "../QnA/Modal";
+import { useSelector } from "react-redux";
 
-//todo add qna modal
 
 const PropertyList = (props)=>{
+  const auth = useSelector((state) => state.auth);
   let navigate = useNavigate();
   
   const [ showModal, setShowModal ] = useState(false);
@@ -114,89 +115,101 @@ const PropertyList = (props)=>{
     };
     fetchData();
   },[props.path, minPrice,maxPrice,minFloorsize,maxFloorsize,minPSF,maxPSF,minYear,maxYear,noOfBathrooms,noOfBedrooms,propertyType,tenure,sellerId]);
-
+  
   return(
-    <Container>
-      <FilterBar propertyType={propertyType} setPropertyType={setPropertyType} noOfBedrooms={noOfBedrooms} setNoOfBedrooms={setNoOfBedrooms} minPrice={minPrice} maxPrice={maxPrice}
-        setMinPrice={setMinPrice} setMaxPrice={setMaxPrice} setMinFloorsize={setMinFloorsize} setMaxFloorsize={setMaxFloorsize} setMinPSF={setMinPSF} setMaxPSF={setMaxPSF}
-        setMinYear={setMinYear} setMaxYear={setMaxYear} setNoOfBathrooms={setNoOfBathrooms} setTenure={setTenure}
-      />
-      <div className={styles.mainContainer}>
-        <div className={styles.cardContainer}>
-          <div>
-            <p>{data.length} Properties {props.path === 'All'? null: `for ${props.path}`} found in Singapore</p>
-            {data.map(propertyData =>{
-              return(
-                <div className={styles.propertyContainer} key = {propertyData.id} onClick={()=>{
-                  if(parseInt(params.sellerId) === propertyData.sellerId){
-                    navigate(`/properties/agent/${params.sellerId}/${propertyData.id}`)
-                  } else {
-                    navigate(`/properties/rent/${propertyData.id}`)
-                  }
-                }}> 
-                  <div style={{height:'480px'}}>
-                    {/* //todo this should come from API */}
-                    <ImageViewer source={[
-                    '/assets/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-1.jpg',
-                    '/assets/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-2.jpg',
-                    '/assets/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-3.jpg',
-                    '/assets/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-4.jpg'
-                    ]}/>
-                  </div>
-                  <div className={styles.propertyCard}>
-                    <p className={styles.fontLargeBold}>{propertyData.propertyName}</p>
-                    <p className={styles.fontSmall}>{propertyData.address}, {propertyData.postcode}, Singapore</p>
-                    <div className={styles.horizontalContainer}>
-                      {propertyData.saleType==='Rent'? <p>S$ {propertyData.price.toLocaleString()} /mo</p>: <p>S$ {propertyData.price.toLocaleString()}</p>}
-                      <p className={styles.availability}>• Available from</p>
+    props.path === 'All' && parseInt(sellerId) !== auth.id? null:
+      <Container>
+        <FilterBar 
+          propertyType={propertyType} setPropertyType={setPropertyType} 
+          noOfBedrooms={noOfBedrooms} setNoOfBedrooms={setNoOfBedrooms} 
+          noOfBathrooms={noOfBathrooms} setNoOfBathrooms={setNoOfBathrooms} 
+          tenure={tenure} setTenure={setTenure}
+          minPrice={minPrice} setMinPrice={setMinPrice}
+          maxPrice={maxPrice} setMaxPrice={setMaxPrice} 
+          minFloorsize={minFloorsize} setMinFloorsize={setMinFloorsize} 
+          maxFloorsize={maxFloorsize} setMaxFloorsize={setMaxFloorsize} 
+          minPSF={minPSF} setMinPSF={setMinPSF} 
+          maxPSF={maxPSF} setMaxPSF={setMaxPSF}
+          minYear={minYear} setMinYear={setMinYear} 
+          maxYear={maxYear} setMaxYear={setMaxYear} 
+
+        />
+        <div className={styles.mainContainer}>
+          <div className={styles.cardContainer}>
+            <div>
+              <p>{data.length} Properties {props.path === 'All'? null: `for ${props.path}`} found in Singapore</p>
+              {data.map(propertyData =>{
+                return(
+                  <div className={styles.propertyContainer} key = {propertyData.id} onClick={()=>{
+                    if(parseInt(params.sellerId) === propertyData.sellerId){
+                      navigate(`/properties/agent/${params.sellerId}/${propertyData.id}`)
+                    } else {
+                      navigate(`/properties/rent/${propertyData.id}`)
+                    }
+                  }}> 
+                    <div style={{height:'480px'}}>
+                      {/* //todo this should come from API */}
+                      <ImageViewer source={[
+                      '/assets/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-1.jpg',
+                      '/assets/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-2.jpg',
+                      '/assets/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-3.jpg',
+                      '/assets/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-4.jpg'
+                      ]}/>
                     </div>
-                    <div className={styles.horizontalContainer}>
-                      <p>{propertyData.noOfBedrooms} <img className={styles.icon} src='/assets/icons/bed-icon.png' alt='bedIcon'></img></p>
-                      <p>{propertyData.noOfBaths} <img className={styles.icon} src='/assets/icons/bath-icon.png' alt='bathIcon'></img></p>
-                      <p>• {propertyData.floorsize} sqft</p>
-                      <p>• S$ {propertyData.pricePSF} psf</p>
-                    </div>
-                    <p>Distance to MRT</p>
-                  </div>
-                  <div className={styles.agentCard}>
-                    <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
-                      <img className={styles.avatar} src={`http://68.183.183.118:4088/img/users/${propertyData.User.photo}`} alt='avatar'></img> 
-                      <div>
-                        <p>Listed by {propertyData.User.firstName} {propertyData.User.lastName}</p>
-                        <p>'Great Property!!'</p>
+                    <div className={styles.propertyCard}>
+                      <p className={styles.fontLargeBold}>{propertyData.propertyName}</p>
+                      <p className={styles.fontSmall}>{propertyData.address}, {propertyData.postcode}, Singapore</p>
+                      <div className={styles.horizontalContainer}>
+                        {propertyData.saleType==='Rent'? <p>S$ {propertyData.price.toLocaleString()} /mo</p>: <p>S$ {propertyData.price.toLocaleString()}</p>}
+                        <p className={styles.availability}>• Available from</p>
                       </div>
+                      <div className={styles.horizontalContainer}>
+                        <p>{propertyData.noOfBedrooms} <img className={styles.icon} src='/assets/icons/bed-icon.png' alt='bedIcon'></img></p>
+                        <p>{propertyData.noOfBaths} <img className={styles.icon} src='/assets/icons/bath-icon.png' alt='bathIcon'></img></p>
+                        <p>• {propertyData.floorsize} sqft</p>
+                        <p>• S$ {propertyData.pricePSF} psf</p>
+                      </div>
+                      <p>Distance to MRT</p>
                     </div>
-                    {/* //* stopPropagation used to prevent parent parent div onClick from being triggered */}
-                    <div className={styles.whatsappButton} onClick={(e)=>{
-                        e.stopPropagation();
-                        window.alert('Link to Whatsapp Web')
-                        }}>
-                      <img className={styles.icon} src='/assets/icons/phone-icon.png' alt='phoneIcon'/>
-                    Whatsapp</div>
+                    <div className={styles.agentCard}>
+                      <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+                        <img className={styles.avatar} src={`http://68.183.183.118:4088/img/users/${propertyData.User.photo}`} alt='avatar'></img> 
+                        <div>
+                          <p>Listed by {propertyData.User.firstName} {propertyData.User.lastName}</p>
+                          <p>'Great Property!!'</p>
+                        </div>
+                      </div>
+                      {/* //* stopPropagation used to prevent parent parent div onClick from being triggered */}
+                      <div className={styles.whatsappButton} onClick={(e)=>{
+                          e.stopPropagation();
+                          window.alert('Link to Whatsapp Web')
+                          }}>
+                        <img className={styles.icon} src='/assets/icons/phone-icon.png' alt='phoneIcon'/>
+                      Whatsapp</div>
+                    </div>
                   </div>
-                </div>
-              )
-            })}
-          </div>
-          {params.sellerId? 
-          <div className={styles.stickySidebar} style={{backgroundColor: 'white'}}>
-            <div className={styles.sidebarSpecificProperty}>
-              <PopupComponent name='Add A Property' propertyData={''}/>
+                )
+              })}
+            </div>
+            {params.sellerId? 
+            <div className={styles.stickySidebar} style={{backgroundColor: 'white'}}>
+              <div className={styles.sidebarSpecificProperty}>
+                <PopupComponent name='Add A Property' propertyData={''}/>
+              </div> 
             </div> 
-          </div> 
-          : 
-          <div className={styles.sidebar}>
-              <img src='/assets/icons/AskGuruLogo.svg' alt='askGuru' className={styles.sidebarImage}></img>
-              <p style={{fontSize:'22px', padding: '10px'}}>How Do I Rent A Property In Singapore</p>
-              <p style={{padding: '10px', fontSize: '14px'}}>Get answers from PropertyLah experts</p>
-              <button className={styles.sidebarButtons} onClick={openModal}>Ask a Question</button>
-              <button className={styles.sidebarButtons} onClick={()=>navigate('/qna')}>Browse Answers</button>
-          </div> 
-          }
+            : 
+            <div className={styles.sidebar}>
+                <img src='/assets/icons/AskGuruLogo.svg' alt='askGuru' className={styles.sidebarImage}></img>
+                <p style={{fontSize:'22px', padding: '10px'}}>How Do I Rent A Property In Singapore</p>
+                <p style={{padding: '10px', fontSize: '14px'}}>Get answers from PropertyLah experts</p>
+                <button className={styles.sidebarButtons} onClick={openModal}>Ask a Question</button>
+                <button className={styles.sidebarButtons} onClick={()=>navigate('/qna')}>Browse Answers</button>
+            </div> 
+            }
+          </div>
+          { showModal ? <Modal setShowModal={setShowModal} /> : null }
         </div>
-        { showModal ? <Modal setShowModal={setShowModal} /> : null }
-      </div>
-    </Container>
+      </Container>
   )
 }
 
